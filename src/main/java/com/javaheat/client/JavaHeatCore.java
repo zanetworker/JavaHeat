5bcbd22ffcbc5ca496114d956f9e0eb4825e92a7
zanetworker <adel.zalok.89@gmail.com>
2016-10-20 12:53:48 +0200

    First Commit JavaHeat

---
+++ src/main/java/com/javaheat/client/JavaHeatCore.java
@@ -0,0 +1,118 @@
+package com.javaheat.client;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpEntityEnclosingRequestBase;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.HttpClientBuilder;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class JavaHeatCore {
+
+    public enum Constants {
+        ENDPOINT("10.10.243.1"),
+        AUTH_PORT("5000"),
+        USERNAME("admin"),
+        PASSWORD("sonata"),
+        TENANT_ID("admin"),
+        AUTHTOKEN_HEADER("X-AUTH-TOKEN"),
+        AUTH_URI("/v2.0/tokens");
+
+        private final String constantValue;
+
+        Constants(String constantValue) {
+            this.constantValue=constantValue;
+        }
+
+        @Override
+        public String toString() {
+            return this.constantValue;
+        }
+    }
+
+    private static HttpEntityEnclosingRequestBase constructRequest(String endpoint,
+                                                                   String port,
+                                                                   String action) {
+        StringBuilder buildUrl = new StringBuilder();
+        buildUrl.append("http://");
+        buildUrl.append(endpoint);
+        buildUrl.append(":");
+        buildUrl.append(port);
+
+        /* TODO : Use regex on the switch statement */
+        switch(action) {
+            case "authenticate":
+                buildUrl.append(Constants.AUTH_URI.toString());
+                break;
+            case "createStack":
+                break;
+            case "checkStatus":
+                break;
+            default:
+                buildUrl.append(Constants.AUTH_URI.toString());
+        }
+
+        System.out.println(buildUrl.toString());
+        return new HttpPost(buildUrl.toString());
+    }
+
+
+    private static String createBody(String action) {
+        String body = null;
+
+        switch(action){
+
+        }
+        return String.format(
+                "{\"auth\": {\"tenantName\": \"%s\", \"passwordCredentials\": {\"username\": \"%s\", \"password\": \"%s\"}}}",
+                Constants.TENANT_ID.toString(),
+                Constants.USERNAME.toString(),
+                Constants.PASSWORD.toString()
+        );
+    }
+
+    public static String sendRequest(String messageType){
+
+        if (messageType != null) {
+            StringBuilder sb = null;
+            HttpClient httpClient = HttpClientBuilder.create().build();
+
+            HttpPost post = (HttpPost) constructRequest(
+                    Constants.ENDPOINT.toString(),
+                    Constants.AUTH_PORT.toString(), messageType);
+
+            StringEntity requestEntity = new StringEntity(createBody(messageType), ContentType.APPLICATION_JSON);
+            post.setEntity(requestEntity);
+            try {
+
+                HttpResponse response = httpClient.execute(post);
+
+                sb = new StringBuilder();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+                String line = null;
+                while ((line = reader.readLine()) != null) {
+                    sb.append(line);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return sb.toString();
+        }
+
+
+        return "";
+    }
+
+
+    public static void main(String [] args ) {
+        System.out.println(sendRequest("authenticate"));
+    }
+
+
+}
---
+++ src/test/java/com/javaheat/client/JavaHeatCoreTest.java
@@ -0,0 +1,7 @@
+package com.javaheat.client;
+
+/**
+ * Created by nle5220 on 20.10.2016.
+ */
+public class JavaHeatCoreTest {
+}
